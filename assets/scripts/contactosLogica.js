be12a15b91dc6a80bde00e4bd6f6033614970ad5
Elmer Santos <esantos-2019254@kinal.edu.gt>
2024-02-03 14:09:12 -0600

    Base de detalle contacto

--- assets/scripts/contactosLogica.js
+++ assets/scripts/contactosLogica.js
@@ -6,3 +6,17 @@ function agregarContacto(){
 function detalleContacto(){
     window.location.href = "detalleContacto.html";
 }
+
+function showDetails(nombre, apellido, edad, telefono, correo) {
+    const detailsContainer = document.getElementById('detailsContainer');
+    const detailsHTML = `
+        <h2>Detalles del Contacto</h2>
+        <p><strong>Nombre:</strong> ${nombre}</p>
+        <p><strong>Apellido:</strong> ${apellido}</p>
+        <p><strong>Edad:</strong> ${edad}</p>
+        <p><strong>Telefono:</strong> ${telefono}</p>
+        <p><strong>Correo:</strong> ${correo}</p>
+    `;
+    // Inserta el HTML en el contenedor
+    detailsContainer.innerHTML = detailsHTML;
+}
